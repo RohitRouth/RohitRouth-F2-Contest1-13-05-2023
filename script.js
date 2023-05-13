f64657ba2171a62c57951b0e697984958c80ef4f
RohitRouth <rohitrouth11@gmail.com>
2023-05-13 20:18:31 +0530

    Completed the given tasks

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-/** @format */
 
-let arr = [
+let students = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
+  { id: 3, name: "karen", age: "19", marks: 35 }
 ];
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
+const PrintStudentbyMap = () => {
+  const studentsWithMarksOver50 = students
+    .filter(student => student.marks > 50)
+    .map(student => student.name);
+  console.log(studentsWithMarksOver50);
+};
 
-function addData() {
-  //Write your code here, just console.log
-}
 
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
+const PrintStudentbyForEach = () => {
+  students.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student.name);
+    }
+  });
+};
 
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+
+const addData = () => {
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  students.push(newStudent);
+  console.log(newStudent);
+};
+
+
+const removeFailedStudent = () => {
+  students = students.filter(student => student.marks >= 50);
+  console.log(students);
+};
+
+
+const ConcatenateArray = () => {
+  const newStudents = [
+    { id: 5, name: "alice", age: "21", marks: 90 },
+    { id: 6, name: "bob", age: "22", marks: 70 },
+    { id: 7, name: "charlie", age: "23", marks: 60 }
+  ];
+  students = students.concat(newStudents);
+  console.log(students);
+};
